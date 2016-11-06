94d79178889931ee26ea892468e90bf36b66ad1c
Andrew Carter <ajc388@nau.edu>
2016-11-06 14:52:42 -0700

    No idea whats wrong with the sort... this is a ui disaster!

--- assets/javascript/movelib.js
+++ assets/javascript/movelib.js
@@ -16,9 +16,29 @@
 
       //Display moves
       $.each(moves, function (type, value) {
-        $("#moveList").append("<div class='blockheader'><h3>"+type+"</h3></div>");
-        //var moveArray = $.map(moves[key], function(el) { return el});;
+        $("#moveList").append(
+            "<div class='blockheader'>"+
+              "<button id='sort_name_"+type+"' class='sort_button ui-button ui-widget ui-corner-all'>Sort By Name</button>"+
+              "<span style='display: inline-block; width: 80%;'>"+type+"</span>"+
+              "<button id='sort_power_"+type+"' class='sort_button ui-button ui-widget ui-corner-all'>Sort By Power</button>"+
+             "</div>"
+            );
+        $(".sort_button").css({
+          display: 'inline-block',
+          width: '120px',
+          backgroundColor: 'darkslateblue',
+          color: 'white'
+        });
+
         displayMoves(type, moves[type], colorMap);
+
+        //bind sort functionality to buttons
+        $("#sort_name_"+type).on('click', function() {
+          sortByName(type);
+        });
+        $("#sort_power_"+type).on('click', function() {
+          sortByPower(type);
+        });
       });
 
       /*Search Functionality*/
@@ -34,7 +54,43 @@
         .on("keyup", function() { search(); })
         .on('autocompleteclose', function () {  search(); });
 
+
     });
+
+    function sortByName(type)
+    {
+        var accordion = $("#accordion"+type);
+        var entries = $.map(accordion.find('.accordionItem'), function(item) {
+          return $(item).add($(item).next());
+        });
+        console.log("here");
+
+        var sort = entries.sort(function (a, b) {
+          return $(a).find(".moveName").text() > $(b).find(".moveName").text();
+        });
+
+        $.each(entries, function() {
+          this.detach().appendTo(accordion);
+        });
+    }
+
+    function sortByPower(type)
+    {
+        var accordion = $("#accordion"+type);
+        $("#accordion"+type).empty();
+
+        var entries = $.map(accordion.find('.accordionItem'), function(item) {
+          return $(item).add($(item).next());
+        });
+
+        var sort = entries.sort(function (a, b) {
+          return $(a).find(".moveValue").text() < $(b).find(".moveValue").text();
+        });
+
+        $.each(entries, function(e) {
+          e.detach().append(accordion);
+        });
+    }
 
     function search()
     {
@@ -73,14 +129,14 @@
     {
       if (Object.keys(moveList).length > 0)
       {
-        $("#moveList").append("<div id='accordion"+type+"'>");
+        var accordion = $("<div id='accordion"+type+"'></div>");
         for (var moveName in moveList)
         {
           var move = moveList[moveName];
           if ( move )
           {
             //Create header div for accordion
-            $("#accordion"+type).append("<div class='moveHeader' id='header_"+moveName+"'>"+
+            accordion.append("<div class='moveHeader accordionItem' id='header_"+moveName+"'>"+
                                        "<img class='icon' id='img_icon_"+moveName+"'"+ 
                                        "src= 'assets/images/type_icons/"+type+".png' ></img>"+
                                        "<span class='moveName'>"+moveName.replace('_', ' ')+"</span>"+
@@ -90,12 +146,12 @@
             //Create dice icons - displayed after name inside the dice span tag
             for ( var i = 1; i < Math.round(move.power/30, 0)+1; i++ )
             {
-              $("#img_dice_"+moveName).append("<img class='icon' src='assets/images/dice_icons/die_"+i+".png'/>");
+              accordion.find("#img_dice_"+moveName).append("<img class='icon' src='assets/images/dice_icons/die_"+i+".png'/>");
             }
 
             //Create content div for accordion
-            $("#accordion"+type)
-            .append("<div class='moveContent' id='content_"+moveName+"'>"+
+            accordion
+            .append("<div class='moveContent accordionItem' id='content_"+moveName+"'>"+
                     "<p><span class='descriptiveHeader'> Power: </span>" + move.power + "</p>" +
                     "<p><span class='descriptiveHeader'> Style: </span>" + move.style + "</p>" +
                     "<p><span class='descriptiveHeader'> Effect: </span>" + move.effect + "</p>"+
@@ -103,11 +159,13 @@
                     "</div>");
             
             //Set colors from colormap
-            $("#header_"+moveName).css('background-color', colorMap.get(type)[0] );
-            $("#content_"+moveName).css('background-color', colorMap.get(type)[1] );
+            accordion.find("#header_"+moveName).css('background-color', colorMap.get(type)[0] );
+            accordion.find("#content_"+moveName).css('background-color', colorMap.get(type)[1] );
           }
         }
-        $("#accordion"+type).accordion();
+
+        $("#moveList").append(accordion);
+        accordion.accordion();
       }
       else 
       {
