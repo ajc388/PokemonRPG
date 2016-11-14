4bbf81cf6bebfe67ea25edad02101c75030884b2
Andrew Carter <ajc388@nau.edu>
2016-11-14 16:24:05 -0700

    Restructure for loop... jquery runs faster... who knew?

--- assets/javascript/movelib.js
+++ assets/javascript/movelib.js
@@ -146,50 +146,45 @@ function displayMoves(type, moveList)
   if (Object.keys(moveList).length > 0)
   {
     var accordion = $("<div id='accordion"+type+"'></div>");
-    for (var moveName in moveList)
-    {
-      var move = moveList[moveName];
-      if ( move )
+    $.each(moveList, function(moveName, move) {
+      var flavor = typeof move.flavor !== "undefined" ? move.flavor : "No text!";
+      var power = typeof move.power !== "undefined" ? move.power : "0";
+
+      //Create header div for accordion
+      var header = $("<div class='moveHeader "+ type.toLowerCase() +"' id='header_"+moveName+"'></div>");
+      header.append("<img class='icon' id='img_icon_"+moveName+"' src= 'assets/images/type_icons/"+type+".png' ></img>");
+      header.append("<span class='moveName'>"+moveName.replace('_', ' ')+"</span>");
+      header.append("<span class='moveDice' id='img_dice_"+moveName+"'></span>");
+      header.append("<span class='moveFlavor'>" + flavor +"</span>");
+      header.append("<span class='movePower'>" + power + "</span>");
+      accordion.append(header);
+      
+      //Create dice icons - displayed after name inside the dice span tag
+      for ( var i = 1; i < Math.round(move.power/30, 0)+1; i++ )
       {
-        var flavor = typeof move.flavor !== "undefined" ? move.flavor : "No text!";
-        var power = typeof move.power !== "undefined" ? move.power : "0";
-
-        //Create header div for accordion
-        var header = $("<div class='moveHeader "+ type.toLowerCase() +"' id='header_"+moveName+"'></div>");
-        header.append("<img class='icon' id='img_icon_"+moveName+"' src= 'assets/images/type_icons/"+type+".png' ></img>");
-        header.append("<span class='moveName'>"+moveName.replace('_', ' ')+"</span>");
-        header.append("<span class='moveDice' id='img_dice_"+moveName+"'></span>");
-        header.append("<span class='moveFlavor'>" + flavor +"</span>");
-        header.append("<span class='movePower'>" + power + "</span>");
-        accordion.append(header);
-        
-        //Create dice icons - displayed after name inside the dice span tag
-        for ( var i = 1; i < Math.round(move.power/30, 0)+1; i++ )
-        {
-          accordion.find("#img_dice_"+moveName).append("<img class='icon' src='assets/images/dice_icons/die_"+i+".png'/>");
-        }
-
-        //Create content div for accordion
-        var content = $("<div class='moveContent' id='content_"+moveName+"'></div>");
-        var style = typeof move.style !== "undefined" ? move.style : "Any";
-        var effect = typeof move.effect !== "undefined" ? move.effect : "No effect";
-        var critical = typeof move.critical !== "undefined" ? move.critical : "";
-        var outofbattle = typeof move.out_of_battle !== "undefined" ? move.out_of_battle : "";
-
-        content.append("<p><span class='descriptiveHeader'> Value: </span><img src='assets/images/pokedollar.png'/>" + Math.pow(power,2) + "</p>");
-        content.append("<p><span class='descriptiveHeader'> Style: </span>" + style + "</p>");
-        content.append("<p><span class='descriptiveHeader'> Effect: </span>" + effect + "</p>");
-        if (critical != "") { content.append("<p><span class='descriptiveHeader'> Critical: </span>" + critical + "</p>"); }
-        if (outofbattle != "") { content.append("<p><span class='descriptiveHeader'> Out Of Battle: </span>" + outofbattle + "</p>"); }
-        accordion.append(content);
+        accordion.find("#img_dice_"+moveName).append("<img class='icon' src='assets/images/dice_icons/die_"+i+".png'/>");
       }
-    }
+
+      //Create content div for accordion
+      var content = $("<div class='moveContent' id='content_"+moveName+"'></div>");
+      var style = typeof move.style !== "undefined" ? move.style : "Any";
+      var effect = typeof move.effect !== "undefined" ? move.effect : "No effect";
+      var critical = typeof move.critical !== "undefined" ? move.critical : "";
+      var outofbattle = typeof move.out_of_battle !== "undefined" ? move.out_of_battle : "";
+
+      content.append("<p><span class='descriptiveHeader'> Value: </span><img src='assets/images/pokedollar.png'/>" + Math.pow(power,2) + "</p>");
+      content.append("<p><span class='descriptiveHeader'> Style: </span>" + style + "</p>");
+      content.append("<p><span class='descriptiveHeader'> Effect: </span>" + effect + "</p>");
+      if (critical != "") { content.append("<p><span class='descriptiveHeader'> Critical: </span>" + critical + "</p>"); }
+      if (outofbattle != "") { content.append("<p><span class='descriptiveHeader'> Out Of Battle: </span>" + outofbattle + "</p>"); }
+      accordion.append(content);
+  });
 
     $("#moveList").append(accordion);
     accordion.accordion({heightStyle: ""});
-  }
-  else 
-  {
-    $("#moveList").append("<p>No results!?!?!</p>");
-  }
+
+  } 
+  else {
+    $("#moveList").append("No data!");
+  } 
 }
